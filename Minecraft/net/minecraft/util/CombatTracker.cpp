cf06df5d2c4c4c96c68e18c8a98c192d2f8ad631
Thurner Mathias <mathias.thurner@cni.at>
2022-08-14 19:25:34 +0200

    Some change

--- Minecraft/net/minecraft/util/CombatTracker.cpp
+++ Minecraft/net/minecraft/util/CombatTracker.cpp
@@ -34,7 +34,8 @@ void CombatTracker::trackDamage(const DamageSource::DamageSource & damageSrc, co
 {
 	reset();
 	calculateFallSuffix();
-	std::shared_ptr<CombatEntry> combatentry = std::make_shared<CombatEntry>(damageSrc, fighter->ticksExisted, healthIn, damageAmount, fallSuffix, fighter->fallDistance);
+    std::unique_ptr<CombatEntry> combatentry = std::make_unique<CombatEntry>(
+        damageSrc, fighter->ticksExisted, healthIn, damageAmount, fallSuffix, fighter->fallDistance);
 	combatEntries.push_back(combatentry);
 	lastDamageTime = fighter->ticksExisted;
 	takingDamage = true;
@@ -46,98 +47,113 @@ void CombatTracker::trackDamage(const DamageSource::DamageSource & damageSrc, co
 	}
 }
 
-ITextComponent* CombatTracker::getDeathMessage()
-{
+std::shared_ptr<ITextComponent> CombatTracker::getDeathMessage() {
 	if (combatEntries.empty()) {
-		return new TextComponentTranslation("death.attack.generic", new Object[]{ this.fighter.getDisplayName() });
+        constexpr std::string_view temp = "death.attack.generic";
+        return std::make_unique<TextComponentTranslation>(temp,fighter->getDisplayName());
 	}
 	else {
-		CombatEntry combatentry = this.getBestCombatEntry();
-		CombatEntry combatentry1 = (CombatEntry)this.combatEntries.get(this.combatEntries.size() - 1);
-		ITextComponent itextcomponent1 = combatentry1.getDamageSrcDisplayName();
-		Entity entity = combatentry1.getDamageSrc().getTrueSource();
-		Object itextcomponent;
-		if (combatentry != nullptr && combatentry1.getDamageSrc() == DamageSource.FALL) {
-			ITextComponent itextcomponent2 = combatentry.getDamageSrcDisplayName();
-			if (combatentry.getDamageSrc() != DamageSource.FALL && combatentry.getDamageSrc() != DamageSource.OUT_OF_WORLD) {
-				if (itextcomponent2 == null || itextcomponent1 != null && itextcomponent2.equals(itextcomponent1)) {
-					if (itextcomponent1 != null) {
-						ItemStack itemstack = entity instanceof EntityLivingBase ? ((EntityLivingBase)entity).getHeldItemMainhand() : ItemStack.EMPTY;
+		auto combatentry = getBestCombatEntry();
+        auto combatentry1 = combatEntries[combatEntries.size() - 1].get();
+		auto itextcomponent1 = combatentry1->getDamageSrcDisplayName();
+		auto entity = combatentry1->getDamageSrc().getTrueSource();
+        std::shared_ptr<ITextComponent> itextcomponent;
+		if (combatentry1->getDamageSrc() == DamageSource::FALL) {
+			auto itextcomponent2 = combatentry->getDamageSrcDisplayName();
+			if (combatentry->getDamageSrc() != DamageSource::FALL && combatentry->getDamageSrc() != DamageSource::OUT_OF_WORLD) {
+				if (itextcomponent2 == nullptr || itextcomponent1 != nullptr && itextcomponent2 == itextcomponent1) {
+					if (itextcomponent1 != nullptr) {
+                        auto itemstack = Util:: instanceof<EntityLivingBase>(entity) ? reinterpret_cast<EntityLivingBase*>(entity)->getHeldItemMainhand()
+                                                       : ItemStack::EMPTY;
 						if (!itemstack.isEmpty() && itemstack.hasDisplayName()) {
-							itextcomponent = new TextComponentTranslation("death.fell.finish.item", new Object[]{ this.fighter.getDisplayName(), itextcomponent1, itemstack.getTextComponent() });
+                            itextcomponent = std::make_unique<TextComponentTranslation>("death.fell.finish.item",
+                                                         fighter->getDisplayName(), itextcomponent1,
+                                                                      itemstack.getTextComponent());
 						}
 						else {
-							itextcomponent = new TextComponentTranslation("death.fell.finish", new Object[]{ this.fighter.getDisplayName(), itextcomponent1 });
+                            itextcomponent = std::make_unique<TextComponentTranslation>("death.fell.finish",
+                                                         fighter->getDisplayName(), itextcomponent1);
 						}
 					}
 					else {
-						itextcomponent = new TextComponentTranslation("death.fell.killer", new Object[]{ this.fighter.getDisplayName() });
+                        itextcomponent = std::make_unique<TextComponentTranslation>("death.fell.killer", fighter->getDisplayName());
 					}
 				}
 				else {
-					Entity entity1 = combatentry.getDamageSrc().getTrueSource();
-					ItemStack itemstack1 = entity1 instanceof EntityLivingBase ? ((EntityLivingBase)entity1).getHeldItemMainhand() : ItemStack.EMPTY;
+					auto entity1 = combatentry->getDamageSrc().getTrueSource();
+                    ItemStack itemstack1 = Util:: instanceof
+                        <EntityLivingBase>(entity1) ? reinterpret_cast<EntityLivingBase*>(entity1)->getHeldItemMainhand()
+                                                    : ItemStack::EMPTY;
 					if (!itemstack1.isEmpty() && itemstack1.hasDisplayName()) {
-						itextcomponent = new TextComponentTranslation("death.fell.assist.item", new Object[]{ this.fighter.getDisplayName(), itextcomponent2, itemstack1.getTextComponent() });
+                        itextcomponent =
+                            std::make_unique <
+                            TextComponentTranslation>("death.fell.assist.item",
+                                                     fighter->getDisplayName(), itextcomponent2,
+                                                                  itemstack1.getTextComponent());
 					}
 					else {
-						itextcomponent = new TextComponentTranslation("death.fell.assist", new Object[]{ this.fighter.getDisplayName(), itextcomponent2 });
+                        itextcomponent =
+                            std::make_unique <
+                            TextComponentTranslation>("death.fell.assist",
+                                                     fighter->getDisplayName(), itextcomponent2);
 					}
 				}
 			}
 			else {
-				itextcomponent = new TextComponentTranslation("death.fell.accident." + this.getFallSuffix(combatentry), new Object[]{ this.fighter.getDisplayName() });
+                itextcomponent = std::make_unique <
+                                 TextComponentTranslation>("death.fell.accident." + getFallSuffix(combatentry),
+                                                          fighter->getDisplayName());
 			}
 		}
 		else {
-			itextcomponent = combatentry1.getDamageSrc().getDeathMessage(this.fighter);
+			itextcomponent = combatentry1->getDamageSrc().getDeathMessage(fighter);
 		}
 
-		return (ITextComponent)itextcomponent;
+		return itextcomponent;
 	}
 }
 
 EntityLivingBase* CombatTracker::getBestAttacker()
 {
-	EntityLivingBase* entitylivingbase;
-	EntityPlayer* entityplayer;
+	EntityLivingBase* entitylivingbase = nullptr;
+    EntityPlayer *entityplayer = nullptr;
 	float f = 0.0F;
 	float f1 = 0.0F;
-	Iterator var5 = combatEntries.iterator();
+    auto ite = combatEntries.begin();
 
 	while (true) {
-		CombatEntry combatentry;
+        CombatEntry* combatentry = nullptr;
 		do {
 			do {
-				if (!var5.hasNext()) {
+                ++ite;
+                if (ite == combatEntries.end()) {
 					if (entityplayer != nullptr && f1 >= f / 3.0F) {
 						return entityplayer;
 					}
 
 					return entitylivingbase;
+				}					
+				combatentry = (*ite).get();
+                if (Util:: instanceof <EntityPlayer>(combatentry->getDamageSrc().getTrueSource()) && (entityplayer == nullptr || combatentry->getDamage() > f1)) {
+					f1 = combatentry->getDamage();
+                    entityplayer = reinterpret_cast < EntityPlayer* > (combatentry->getDamageSrc().getTrueSource());
 				}
+            } while (!(Util::instanceof<EntityLivingBase>(combatentry->getDamageSrc().getTrueSource())));
+		} while (entitylivingbase != nullptr && combatentry->getDamage() <= f);
 
-				combatentry = (CombatEntry)var5.next();
-				if (combatentry.getDamageSrc().getTrueSource() instanceof EntityPlayer && (entityplayer == null || combatentry.getDamage() > f1)) {
-					f1 = combatentry.getDamage();
-					entityplayer = (EntityPlayer)combatentry.getDamageSrc().getTrueSource();
-				}
-			} while (!(combatentry.getDamageSrc().getTrueSource() instanceof EntityLivingBase));
-		} while (entitylivingbase != null && combatentry.getDamage() <= f);
-
-		f = combatentry.getDamage();
-		entitylivingbase = (EntityLivingBase)combatentry.getDamageSrc().getTrueSource();
+		f = combatentry->getDamage();
+        entitylivingbase = reinterpret_cast < EntityLivingBase*>(combatentry->getDamageSrc().getTrueSource());
 	}
 }
 
-int32_t CombatTracker::getCombatDuration() const
+uint32_t CombatTracker::getCombatDuration() const
 {
 	return inCombat ? fighter->ticksExisted - combatStartTime : combatEndTime - combatStartTime;
 }
 
 void CombatTracker::reset()
 {
-	int32_t i = inCombat ? 300 : 100;
+	uint32_t i = inCombat ? 300 : 100;
 	if (takingDamage && (!fighter->isEntityAlive() || fighter->ticksExisted - lastDamageTime > i)) {
 		bool flag = inCombat;
 		takingDamage = false;
@@ -156,9 +172,9 @@ EntityLivingBase* CombatTracker::getFighter() const
 	return fighter;
 }
 
-std::string CombatTracker::getFallSuffix(CombatEntry entry)
+std::string CombatTracker::getFallSuffix(CombatEntry* entry)
 {
-	return entry.getFallSuffix().empty() ? "generic" : entry.getFallSuffix();
+	return entry->getFallSuffix().empty() ? "generic" : entry->getFallSuffix();
 }
 
 void CombatTracker::resetFallSuffix()
@@ -166,17 +182,17 @@ void CombatTracker::resetFallSuffix()
 	fallSuffix.clear();
 }
 
-CombatEntry CombatTracker::getBestCombatEntry()
+CombatEntry* CombatTracker::getBestCombatEntry()
 {
-	CombatEntry combatentry;
-	CombatEntry combatentry1;
+    CombatEntry* combatentry = nullptr;
+    CombatEntry *combatentry1 = nullptr;
 	float f = 0.0F;
 	float f1 = 0.0F;
 
-	for (int i = 0; i < combatEntries.size(); ++i) {
-		CombatEntry combatentry2 = (CombatEntry)combatEntries.get(i);
-		CombatEntry combatentry3 = i > 0 ? (CombatEntry)combatEntries.get(i - 1) : null;
-		if ((combatentry2.getDamageSrc() == DamageSource.FALL || combatentry2.getDamageSrc() == DamageSource.OUT_OF_WORLD) && combatentry2.getDamageAmount() > 0.0F && (combatentry == null || combatentry2.getDamageAmount() > f1)) {
+	for (size_t i = 0; i < combatEntries.size(); ++i) {
+        auto combatentry2 = combatEntries[i].get();
+        auto combatentry3 = i > 0 ? combatEntries[(i - 1)].get() : nullptr;
+		if ((combatentry2->getDamageSrc() == DamageSource::FALL || combatentry2->getDamageSrc() == DamageSource::OUT_OF_WORLD) && combatentry2->getDamageAmount() > 0.0F && (combatentry == nullptr || combatentry2->getDamageAmount() > f1)) {
 			if (i > 0) {
 				combatentry = combatentry3;
 			}
@@ -184,22 +200,23 @@ CombatEntry CombatTracker::getBestCombatEntry()
 				combatentry = combatentry2;
 			}
 
-			f1 = combatentry2.getDamageAmount();
+			f1 = combatentry2->getDamageAmount();
 		}
 
-		if (combatentry2.getFallSuffix() != null && (combatentry1 == null || combatentry2.getDamage() > f)) {
+		if (!combatentry2->getFallSuffix().empty() &&
+            (combatentry1 == nullptr || combatentry2->getDamage() > f)) {
 			combatentry1 = combatentry2;
-			f = combatentry2.getDamage();
+			f = combatentry2->getDamage();
 		}
 	}
 
-	if (f1 > 5.0F && combatentry != null) {
-		return combatentry;
+	if (f1 > 5.0F && combatentry != nullptr) {
+        return combatentry;
 	}
-	else if (f > 5.0F && combatentry1 != null) {
-		return combatentry1;
+	else if (f > 5.0F && combatentry1 != nullptr) {
+        return combatentry1;
 	}
 	else {
-		return null;
+        return nullptr;
 	}
 }
