d08672a777cfecbbb9cd6d50f3eb7d8e51db5a28
Thurner Mathias <mathias.thurner@cni.at>
2022-09-21 13:56:14 +0200

    isAllowedCharacter from $ to &

--- Minecraft/net/minecraft/util/ChatAllowedCharacters.cpp
+++ Minecraft/net/minecraft/util/ChatAllowedCharacters.cpp
@@ -3,7 +3,7 @@
 
 constexpr bool ChatAllowedCharacters::isAllowedCharacter(char character)
 {
-	return character != '§' && character >= ' ' && character != 127;
+	return character != '&' && character >= ' ' && character != 127;
 }
 
 std::string ChatAllowedCharacters::filterAllowedCharacters(std::string_view input)
